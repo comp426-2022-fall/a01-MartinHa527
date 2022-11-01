f757bbffa16b02e1fd0ac8c56400ce233569f643
root <root@DESKTOP-A31BLJ7.localdomain>
2022-10-31 22:55:45 -0400

    1

--- server.js
+++ server.js
@@ -29,7 +29,30 @@
 // 3. end with the data that you are reading in from ./public/index.html.
 
 
+const http = require('http')
+const fs = require('fs')
+const hostname = '127.0.0.1'
 
+const minimist = require('minimist');
+var args = minimist(process.argv);
+const port = args.port || 3000;
+
+const server = http.createServer((req, res) => {
+    var data;
+        try {
+            data = fs.readFileSync('./public/index.html', 'utf8');
+            // console.log(data);
+          } catch (err) {
+            console.error(err);
+          }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server listening on port ${port}`)
+})
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
